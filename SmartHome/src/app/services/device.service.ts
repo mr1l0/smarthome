8d7d94ceb6860132750af0e2b286ddd3360711b4
Murilo Marcolino Amaral <muriloamaral1@gmail.com>
2019-10-20 19:13:32 -0300

    Alterações no device light

---
+++ SmartHome/src/app/services/device.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { DeviceComponent } from '../dataModel/device-component';
+import { Device } from '../dataModel/device';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeviceService {
+
+  constructor() { }
+}
--- SmartHome/src/app/viewFactory/devices/light/light.component.ts
+++ SmartHome/src/app/viewFactory/devices/light/light.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Device } from 'src/app/dataModel/device';
+import { CallHardwareService } from 'src/app/services/call-hardware.service';
+import { DeviceComponent } from 'src/app/dataModel/device-component';
 
 @Component({
   selector: 'app-light',
@@ -8,10 +10,16 @@ import { Device } from 'src/app/dataModel/device';
 })
 export class LightComponent<T> implements OnInit {
 
-  constructor() { }
+  constructor(
+    protected callHardwareService: CallHardwareService
+  ) { }
 
-  @Input() device: {new(device: Device): T;};
-  
-  ngOnInit() {
+  @Input() device: {new(device: Device): T;};  
+
+  ngOnInit() {    
+    let rele: DeviceComponent = this.device._source.components.find(result => result._source.name == 'relé');        
+    this.callHardwareService.read(rele).subscribe((result: any) => {
+      rele._source.get = result.value;
+    });    
   }
 }
